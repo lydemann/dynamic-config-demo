0c6b5a5609efb63f9b61b650bd270addf50bc853
Christian Lüdemann <christianlydemann@gmail.com>
2018-09-10 14:34:04 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-config-demo';
+
+  public get environmentName() {
+    return environment.environment;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AppInitService } from './app-init.service';
 
+export function init_app(appLoadService: AppInitService) {
+  return () => appLoadService.init();
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +13,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AppInitService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: init_app,
+      deps: [AppInitService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
